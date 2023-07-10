27384c4f4c2c13661021bbcc7a12d85ea9583807
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-07-10 13:45:30 -0600

    Fixed . bug

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -29,31 +29,27 @@ return (
         type='text'
         placeholder='Add Task'
         value={text}
-        onChange={(e) => setText(e.target.
-        value)}
+        onChange={(e) => setText(e.target.value)}
     />
     </div>
     <div className='form-control'>
-       <label>Day & Time</label>
-       <input 
+      <label>Day & Time</label>
+      <input 
         type='text'
         placeholder='Add Day & Time'
         value={day}
-        onChange={(e) => setDay(e.target.
-        value)}
-        />
-      </div>
-      <div className='form-control
-      form-control-check'>
-        <label class ="reminder">Set Reminder</label>
-        <input
-            type='checkbox'
-            checked={reminder}
-            value={reminder}
-            onChange={(e) => setReminder(e.
-            currentTarget.checked)} 
-       />
-      </div>
+        onChange={(e) => setDay(e.target.value)}
+      />
+    </div>
+    <div className='form-control form-control-check'>
+      <label class ="reminder">Set Reminder</label>
+      <input
+        type='checkbox'
+        checked={reminder}
+        value={reminder}
+        onChange={(e) => setReminder(e.currentTarget.checked)} 
+      />
+    </div>
 
       <input type='submit' value='Save Task'
         className='btn btn-block' />
